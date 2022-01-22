a5d0fa8d0b863c4ff4877c7fca85df535bd18b5f
Martin Bridgman <martin.bridgman@gmail.com>
2022-01-22 00:47:34 +0000

    completed examples

--- map.js
+++ map.js
@@ -5,15 +5,38 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for(let num of nums){
+  results.push(num * 2);
+}
+console.log(results);
 
 
 // Using map()
 
 
-// Simplified w/ map()
+//Example one: Use Map with callback function called from within map method parameters.
+const multByTwo = function (num){
+  return num * 2
+}
+
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 
-// Simplfied w/ map() + arrow function
+
+//Example two: Use Map with callback function within map method parameters.
+const simplified = nums.map(function(num){return num * 2});
+console.log(simplified);
+
+
+
+//Example three: Use Map with callback arrow function within map method parameters.
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
+
+
 
 
 // With objects:
@@ -37,3 +60,16 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+// iterate object with for loop
+const studentsWithIds = [];
+for( let student of students){
+  studentsWithIds.push([student.id, student.name]);
+
+}
+console.log(studentsWithIds);
+
+// iterate object with map method
+const mapStudentsWithIds = students.map(student => [student.id, student.name]);
+
+console.log(mapStudentsWithIds);
